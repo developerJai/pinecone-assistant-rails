87d81b48c7a73cdaa635810606e8b3e8d41592fe
Jai Rajput <jai@tecorb.co>
2025-01-03 09:53:18 +0530

    Chat with pinecone assistant

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,23 @@
+class ChatsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
+  def ask
+    unless params[:question].present?
+      render json: { error: "No question provided" }, status: :bad_request
+      return
+    end
+
+    answer = chat_with_assistant(params[:question])
+    render json: { answer: answer }
+  rescue StandardError => e
+    Rails.logger.error("Error in chat: #{e.message}")
+    render json: { error: "Internal server error" }, status: :internal_server_error
+  end
+
+  private
+
+  def chat_with_assistant(question)
+    response = Pinecone::Assistant::Chat.new.call(question)
+    response.dig("message", "content")
+  end
+end
---
+++ app/services/pinecone/assistant/chat.rb
@@ -0,0 +1,33 @@
+class Pinecone::Assistant::Chat
+  require "uri"
+  require "json"
+  require "net/http"
+
+  def initialize
+    @assistant_name = ENV["ASSISTANT_NAME"]
+    @pinecone_api_key = ENV["PINECONE_API_KEY"]
+  end
+
+  def call(prompt)
+    url = URI("https://prod-1-data.ke.pinecone.io/assistant/chat/#{@assistant_name}")
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["Content-Type"] = "application/json"
+    request["Api-Key"] = @pinecone_api_key
+    request.body = JSON.dump({
+      "messages": [
+        {
+          "role": "user",
+          "content": prompt
+        }
+      ],
+      "stream": false,
+      "model": "gpt-4o"
+    })
+
+    response = https.request(request)
+    JSON.parse response.body
+  end
+end
